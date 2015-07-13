fb97c2b3bbaf287e03a433ae4a016ebab56e20e5
Guillaume Havard <guill.havard@gmail.com>
2015-07-13 17:29:27 +0200

    Parsing basique des arguments

--- bipbip/bipbip.py
+++ bipbip/bipbip.py
@@ -8,15 +8,26 @@ Fait des bips pour délimiter, en boucle, une période t1 suivi d'une période t
 le tout sur une durrée limité.
 Par exemple une répétition de 30s, 10s, 30s, 10s ... le tout sur 10 minutes
 
->>> python3 bipbip.py t1 t2 ttotal
-or
->>> python3 bipbip.py
-Then you will be asked to give your 3 periods
-
-In both cases the timer will be ready.
-You can launch/pause the timer with 'sapce' or reload it with 'r'.
-
 """
+
+help = """Le nom de mon programme trop coolbipbip
+
+Timer that will chain twwo period 'time1' and 'time2' until the 'time_totel'
+id finish.
+Time in s by default, if in minute add 'm' : 10m
+ 
+Usage:
+  bipbip.py <time1> <time2> <time_total>
+  bipbip.py 
+ 
+Options:
+  -h --help          
+  
+ 
+Once launched you canlaunch/pause the timer with 'space' 
+or reload it with 'r'.
+"""
+
 
 # pygame : 
 #    sons
@@ -30,18 +41,60 @@ You can launch/pause the timer with 'sapce' or reload it with 'r'.
 # Pour les deux http://fsincere.free.fr/isn/python/cours_python_tkinter.php
 
 # Je commence par le son (voir si automatiquement en thread ou s'il faut 
-# l'ajouter)
+# l'ajouter) -> il faut l'ajouter
+# Pseudo module de son fonctionant.
+# Pour la récupération des arguments je vais utiliser docopt
+
+ 
+from docopt import docopt
 
 import sound
 import time
 
 manager = sound.SoundManager()
-manager.play_end_pause()
 
-print("Pause")
-time.sleep(5)
-print("fin")
+arguments = docopt(help)
+print(arguments)
+print(type(arguments["<time1>"]))
+
+def parse_time(time) :
+    """
+    take a string time and convert it in seconds (int).
+    
+    Can detect if the time is in minute '10m' and convert it in seconds.
+    
+    >>> parse_time("-5")
+    0
+    >>> parse_time("10")
+    10
+    >>> parse_time("10m")
+    600
+    >>> parse_time(10)
+    10
+    
+    TODO : voir pour retourner une erreur si la valeur en entrée n'est pas 
+    bonne
+    
+    """
+    ret = 0
+    
+    if isinstance( time, int ):
+        return time
+        
+    if time.isdigit():
+        ret = int(time)
+        if ret < 0 : ret = 0
+    else :
+        if time[:-1].isdigit() and time[-1] == "m":
+            ret = int(time[:-1]) * 60
+    
+    return ret
+    
+if arguments["<time1>"]:
+    print("coucou")
 
 
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
